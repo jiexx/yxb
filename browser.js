6629a074daf2c8d2de0e006291832f613b82f526
jiexx <jiexx@outlook.com>
2017-10-18 20:32:30 +0800

    browser

--- browser.js
+++ browser.js
@@ -1,9 +1,10 @@
 var browser = require('casper').create({
 		pageSettings : {
-			loadImages : false,
-			loadPlugins : false,
+			loadImages : true,
+			loadPlugins : true,
 			javascriptEnabled : true,
 			resourceTimeout : 50000,
+			webSecurityEnabled: false,
 			//userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.21 (KHTML, like Gecko) Chrome/25.0.1349.2 Safari/537.21'
 			userAgent : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'
 		}
@@ -12,23 +13,23 @@ var browser = require('casper').create({
 	});
 phantom.outputEncoding = "GBK";
 browser.options.retryTimeout = 20;
-browser.options.waitTimeout = 20000;
+browser.options.waitTimeout = 200000;
 browser.options.onResourceRequested = function (C, requestData, request) {
-	if (!(/.*\.xunleitai\.com.*/gi).test(requestData['url']) && !(/http:\/\/127\.0\.0\.1.*/gi).test(requestData['url'])
-		/*|| (/.*\.css/gi).test(requestData['url']) || requestData['Content-Type'] == 'text/javascript'*/
-	)
-	{
-		console.log('redirect Skipping JS file: ' + requestData['url']);
-		request.abort();
-	} else {
+	//if (!(/.*\.xunleitai\.com.*/gi).test(requestData['url']) && !(/http:\/\/127\.0\.0\.1.*/gi).test(requestData['url'])
+	//	|| (/.*\.css/gi).test(requestData['url']) || requestData['Content-Type'] == 'text/javascript'
+	//)
+	//{
+	//	console.log('redirect Skipping JS file: ' + requestData['url']);
+	//	request.abort();
+	//} else {
 		console.log('redirect Down JS file: ' + requestData['url']);
-	}
+	//}
 };
 
 if (browser.cli.args.length != 2) {
 	console.log('Usage: browser.js <uid> <AD.>');
 	browser.exit();
-}
+}else {
 
 var fs = require('fs');
 
@@ -52,33 +53,41 @@ browser.on("page.created", function () {
 
 browser.start();
 
-browser.thenOpen('http://wx.qq.com');
-console.log('thenOpen');
+browser.thenOpen('https://wx.qq.com');
+console.log('Open');
 
 browser.waitFor(function check() {
 	return this.evaluate(function () {
 		var a = document.querySelectorAll('div.qrcode').length > 0;
-		//console.log(a);
+		console.log(document.querySelector('div.qrcode img').src);
 		return a;
 	});
 }, function () {
-	this.download(document.querySelector('div.qr img').src, 'keeper/' + browser.cli.args[0]'.jpg');
+	console.log(browser.getElementAttribute('div.qrcode img', 'src'));
+	browser.download(browser.getElementAttribute('div.qrcode img', 'src'), 'qrcode/' + browser.cli.args[0] + '.jpg');
 	console.log('COMMAND: UPDATE');
 	browser.waitFor(function check() {
 		return this.evaluate(function () {
 			var a = document.querySelectorAll('a.btn.btn_send').length > 0;
-			//console.log(a);
+			console.log('====='+document.querySelector('a.btn.btn_send').outerHTML);
 			return a;
 		});
 	}, function () {
 		console.log('COMMAND: DOING');
-		var e = document.querySelectorAll('a.chat_item.slide-left');
+		var e = browser.getElementsInfo('a.chat_item.slide-left');
 		var i = 0;
-		this.repeat(e.length, function () {
-			this.click('#links li:nth-child(' + i + ') a');
+		e.forEach(function (element) {
+			browser.click('a.chat_item.slide-left:nth-child(' + i + ')');
+			browser.waitFor(function check() {
+				return this.evaluate(function () {
+					var a = document.querySelectorAll('a.btn.btn_send').length > 0;
+					return a;
+				});
+			}, function () {
+				browser.sendKeys('pre#editArea', browser.cli.args[1]);
+				browser.click('a.btn.btn_send');
+			});
 			i++;
-			document.querySelector('pre#editArea').innerHTML = browser.cli.args[1];
-			this.click('a.btn.btn_send');
 			if (i == e.length) {
 				console.log('COMMAND: DONE');
 			}
@@ -92,3 +101,4 @@ browser.then(function () {
 });
 
 browser.run();
+}
--- keeper.js
+++ keeper.js
@@ -1,4 +1,4 @@
-//var havename = $(¡®name¡¯).value||undefined;
+
 var socket = io.connect('http://localhost:3000'); 
 
 socket.on('UPDATE', function (data) {
