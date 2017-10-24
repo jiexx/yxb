96d1c5a17f9d39bba212967e26c78eaa49098211
roger <jiexx@outlook.com>
2017-10-24 18:44:06 +0800

    web wrapper

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ var io = require('socket.io')(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
+app.set('view engine', 'ejs');
+app.set('view options', {
+    layout: false
+});
+
+app.use(express.static(__dirname + '/keeper'));
+
 var server = app.listen(3000, function(){
     console.log("Listening on port %s...", server.address().port);
 });
--- router.js
+++ router.js
@@ -1,12 +1,47 @@
+var fs = require("fs");
+var ejs = require("ejs");
+var qr = require("qr-image");
+var url = require("url");
+
 var appRouter = function(app) {
+	app.get("/", function(req, res){
+		//res.sendFile(path.join(__dirname, './', 'sign.html'));
+		var path = url.parse(req.url).pathname.substr(1);
+		if(/^[0-9a-z]$/i.test(path)) {
+			res.redirect('.keeper/'+path);
+		}else {
+			res.redirect('')
+		}
+	});
     app.get("/keeper", function(req, res){
-		res.sendFile(path.join(__dirname, './', 'keeper.html'));
+		//res.sendFile(path.join(__dirname, './', 'sign.html'));
+		res.redirect('./sign.html?uid=123456')
 	});
 	app.post("/keeper/sign", function(req, res){
 		//1.build uid, save advertising txt in /server.wxyxb.com/keeper/ad/req.body.adv
 		//2.build QR*(http://www.wxyxb.com/keeper/req.body.uid)
 		//3.save http://www.wxyxb.com/UID(include QR*), use template keeper.html
-		res.send("");
+		if( !req.body.ad || !req.body.uid ) {
+			res.send('{err:400}');
+		}else {
+			var tmpl = fs.readFileSync('keeper.html','utf-8');
+			var keeper = ejs.render(tmpl, {uid: req.body.uid});
+			var qr_svg = qr.image('http://www.wxyxb.com/keeper/'+req.body.uid, {type:'svg'});
+			qr_svg.pipe(fs.createWriteStream('./qr/'+req.body.uid+'.svg'));
+			fs.appendFile('./keeper/'+req.body.uid, keeper, function (err) {
+				if(!err) {
+					fs.appendFile('./keeper/ad/'+req.body.uid, req.body.ad+'\n', function (err) {
+						if(!err) {
+							res.redirect('./keeper/'+req.body.uid);
+						}else {
+							res.send('{err:401}');
+						}
+					});
+				}else {
+					res.send('{err:402}');
+				}
+			});
+		}
 	});
 }
 module.exports = appRouter;
